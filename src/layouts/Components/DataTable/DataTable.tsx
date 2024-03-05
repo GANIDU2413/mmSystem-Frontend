e88655abb2ccac6796d5cbfbe2a83a7cd52678d2
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-03-06 01:52:30 +0530

    updated 01.52

--- src/layouts/Components/DataTable/DataTable.tsx
+++ src/layouts/Components/DataTable/DataTable.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import * as styles from './DataTable.module.css'
-import { ALL } from 'dns'
+
+import { Checkbox } from '@mui/material'
+import './dataTable.css';
+
 
 export default function DataTable() {
   return (
@@ -12,7 +13,7 @@ export default function DataTable() {
         <th colSpan={100} style={{textAlign:"center"}}>Student Results</th>
     </tr>
     <tr>
-      <th scope="col">#</th>
+      <th scope="col">Select</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
       <th scope="col">Handle</th>
@@ -20,25 +21,67 @@ export default function DataTable() {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
     </tr>
+    <tr>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1 <Checkbox></Checkbox> </th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+
   </tbody>
 </table>
+
+    <div className='right-aligned-div'>
+        <button type="button" className="btn btn-outline-primary">Update Medical</button>&nbsp;&nbsp;
+        <button type="button" className="btn btn-outline-primary">Approve</button>
+    </div>
     </div>
     </center>
   )
