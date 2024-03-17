c6be6b8449cd038ad37829a507af3ded4783813e
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-03-17 11:59:21 +0530

    Update ViewMarks.jsx

--- src/layouts/AR/ViewMarks/ViewMarks.jsx
+++ src/layouts/AR/ViewMarks/ViewMarks.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import DataTable from '../../Components/DataTable/DataTable'
+import MarksTable from '../../MarksTable/MarksTable'
 
 export default function ViewMarks() {
   return (
     <div>
-        <DataTable /> 
+        <MarksTable/>
     </div>
   )
 }
