d45c97066ef7dcf817a096b2000e37ae07424495
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-03-22 09:46:25 +0530

    Stable version 24/03/22 09:46am

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ export const App = () => {
           <Route path="/arviewmtdmarks">
             <ViewMarks department_id={"Multi_Disciplinary"}/>
           </Route>
-          <Route path="/viewMarksRemainingToApprove/:course_id/:course_name">
+          <Route path="/viewMarksRemainingToApprove/:course_id/:course_name/:previousRole">
             <DataTable/>
           </Route>
 
