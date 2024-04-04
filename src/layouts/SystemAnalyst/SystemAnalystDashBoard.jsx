9e3048d0324d0cf4fbe6fae3508df7d58b534f86
GANIDU2413 <ganidusahan@gmail.com>
2024-04-04 06:11:18 +0530

    added

---
+++ src/layouts/SystemAnalyst/SystemAnalystDashBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SystemAnalystDashBoard() {
+  return (
+    <>
+        <div className=' container'>
+            <div className=' py-4'>
+                <h2 className=' mt-lg-4'>Hello, System Analyst...</h2>
+            </div>
+        </div>
+    </>
+  )
+}
