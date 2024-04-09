f0e1aa5afd0740e41d0e970881ae889e5de47582
GANIDU2413 <ganidusahan@gmail.com>
2024-04-10 00:12:05 +0530

    changes

--- src/layouts/SystemAnalyst/AttendenceSA.jsx
+++ src/layouts/SystemAnalyst/AttendenceSA.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function AttendenceSA() {
   return (
-    <div>AttendenceSA</div>
+    <>
+      <div className=' container'>
+        <div className=' col'>
+          <div className=' row-cols-1'>
+            <form action="">
+              
+            </form>
+          </div>
+          <div className=' row-cols-1'>
+            <table className=' table table-hover'>
+              <thead>
+                <tr>
+                  <th scope='col'>#</th>
+                  <th scope='col'>Medical ID</th>
+                  <th scope='col'>Medical state</th>
+                  <th scope='col'>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope='row'>hi</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
