cb1c8d126cfe6a500e9a51db673f55f8dc0738cd
GANIDU2413 <ganidusahan@gmail.com>
2024-04-26 14:40:24 +0530

    Update MainNavebar.tsx

--- src/layouts/NavbarAndFooter/MainNavebar.tsx
+++ src/layouts/NavbarAndFooter/MainNavebar.tsx
@@ -8,25 +8,28 @@ import { Navebar } from '../../Lecture/layouts/NavbarAndFooter/Navebar';
 export default function MainNavbar() {
   const { authState } = useOktaAuth();
 
-  if (authState?.accessToken?.claims.userType === 'ar') {
-    return <NavebarAR />;
-  } else if (authState?.accessToken?.claims.userType === 'hod') {
-    // Add code for HOD
-  } else if (authState?.accessToken?.claims.userType === 'sa') {
-    return <NavebarSA />;
-  } else if (authState?.accessToken?.claims.userType === 'lecturer') {
-    // Add code for lecturer
-    return <Navebar />;
-  } else if (authState?.accessToken?.claims.userType === 'student') {
-    // Add code for student
-  } else if (authState?.accessToken?.claims.userType === 'admin') {
-    // Add code for admin
-  } else if (authState?.accessToken?.claims.userType === 'dean') {
-    // Add code for dean
+  const getNavbar = () => {
+    if (authState?.accessToken?.claims.userType === 'ar') {
+      return <NavebarAR />;
+    } else if (authState?.accessToken?.claims.userType === 'hod') {
+      // Add code for HOD
+    } else if (authState?.accessToken?.claims.userType === 'sa') {
+      return <NavebarSA />;
+    } else if (authState?.accessToken?.claims.userType === 'lecturer') {
+      // Add code for lecturer
+      return <Navebar />;
+    } else if (authState?.accessToken?.claims.userType === 'student') {
+      // Add code for student
+    } else if (authState?.accessToken?.claims.userType === 'admin') {
+      // Add code for admin
+    } else if (authState?.accessToken?.claims.userType === 'dean') {
+      // Add code for dean
+    }
   }
 
   return (
-    <>
-    </>
+    <div>
+      {getNavbar()}
+    </div>
   );
 }
