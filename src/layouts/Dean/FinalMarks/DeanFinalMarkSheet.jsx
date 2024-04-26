e172ea8950d21ca825def4ddbb678c3291d6f001
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2024-04-26 11:25:05 +0530

    Update DeanFinalMarkSheet.jsx

--- src/layouts/Dean/FinalMarks/DeanFinalMarkSheet.jsx
+++ src/layouts/Dean/FinalMarks/DeanFinalMarkSheet.jsx
@@ -3,7 +3,23 @@ import { NavebarDean } from '../NavebarDean'
 import axios from 'axios';
 
 export default function DeanFinalMarkSheet() {
-    const [finalResults,setFinalResults] = useState([]);
+    const [finalResults,setFinalResults] = useState([
+        {
+            student_id:"",
+            courses:[
+                {
+                    course_id: course_id,
+                    overall_score: overall_score,
+                    grade: grade,
+                }
+            ],
+            sgpa:"",
+            cgpa:"",
+        }
+    ]);
+
+    const[student,setStudent]=useState([]);
+    
 
     let level=3
     let sem=1
@@ -15,9 +31,27 @@ export default function DeanFinalMarkSheet() {
         setFinalResults(result.data);
         console.log(result.data);
 
+
+
         finalResults.map((ele)=>
         {
-                
+                setFinalResults(
+                    [
+                       
+                        {
+                            student_id:ele.student_id,
+                            courses:[
+                                {
+                                    course_id: course_id,
+                                    overall_score: overall_score,
+                                    grade: grade,
+                                }
+                            ],
+                            sgpa:null,
+                            cgpa:null,
+                        }
+                    ]
+                )
         })
 
 
