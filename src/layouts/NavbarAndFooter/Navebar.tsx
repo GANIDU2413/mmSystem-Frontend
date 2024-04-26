f4394bb886ba3b478542946213438d39b50268ef
GANIDU2413 <ganidusahan@gmail.com>
2024-04-26 14:14:38 +0530

    Update Navebar.tsx

--- src/layouts/NavbarAndFooter/Navebar.tsx
+++ src/layouts/NavbarAndFooter/Navebar.tsx
@@ -1,134 +1,29 @@
-import { Link, NavLink } from "react-router-dom";
+import React from 'react'
 import { useOktaAuth } from "@okta/okta-react";
-import { SpinerLoading } from "../Utils/SpinerLoading";
+import { NavebarSA } from '../SystemAnalyst/NavebarSA';
 
-export const Navebar = () => {
 
-  const {oktaAuth , authState} = useOktaAuth();
+export default function Navebar() {
+  const { authState } = useOktaAuth();
 
-  if(!authState){
-    return <SpinerLoading/>
+  if (authState?.accessToken?.claims.userType === 'ar') {
+    <NavebarSA />
+  }else if(authState?.accessToken?.claims.userType === 'hod'){
+    
+  }else if(authState?.accessToken?.claims.userType === 'sa'){
+    <NavebarSA />
+  }else if(authState?.accessToken?.claims.userType === 'lecturer'){
+    <Navebar/>
+  }else if(authState?.accessToken?.claims.userType === 'student'){
+
+  }else if(authState?.accessToken?.claims.userType === 'admin'){ 
+
+  }else if(authState?.accessToken?.claims.userType === 'dean'){
+  
   }
 
-  const handleLogout = async () => oktaAuth.signOut();
-
-  console.log(authState);
   return (
-    <nav className="navbar bg-primary fixed-top">
-      <div className="container-fluid">
-        <a
-          className="navbar-brand text-white"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-          href="#"
-        >
-          Faculty Of Technology, Marking Management System 
-        </a>
-        <ul className="navbar-nav ms-auto">
-           {!authState.isAuthenticated?
-               <li className="nav-item m-1">
-               <Link type="button" className="btn btn-outline-light" to='/login'>
-                 Sign in
-               </Link>
-             </li>
-
-             :
-             <li>
-              <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
-             </li>
-          
-          }
-           
-
-          </ul>
-        <div
-          className="offcanvas offcanvas-start"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-              Hi, Mr.Nuwan Laksisri
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                 Dashboard
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Marks Feeding
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Final Marks
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Level I Sem I
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level I Sem II
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level II Sem I
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level II Sem II
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level III Sem I
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level III Sem II
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level IV Sem I
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                    Level IV Sem II
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            
-            <hr></hr>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
+    <>
+    </>
+  )
+}
