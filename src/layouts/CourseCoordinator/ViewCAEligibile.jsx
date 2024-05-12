f4061fad4756561d76f8b70a8d1bdc4cc5daf8b5
GANIDU2413 <ganidusahan@gmail.com>
2024-05-12 16:38:54 +0530

    Update ViewCAEligibile.jsx

--- src/layouts/CourseCoordinator/ViewCAEligibile.jsx
+++ src/layouts/CourseCoordinator/ViewCAEligibile.jsx
@@ -1,41 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import NavBarCC from './NavBarCC';
+import { useOktaAuth } from "@okta/okta-react";
 import axios from 'axios';
-import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
 export default function ViewCAEligibile() {
-    const [course_ID,getCourse_ID] = useState();
-    const [cAMarks,getCAMarks] = useState([]);
+    const [courses, setCourses] = useState([]);
+    const { authState } = useOktaAuth();
+    const history = useHistory();
 
-    const getCourseIDFromcctbl = async()=>{
-        const cidforcc = await axios.get(`http://localhost:9090/api/ccmanage/getcidbyuserid/${user_id}`)
-    }
+    const usernameofcc = authState?.idToken?.claims.preferred_username;
+    // authState?.idToken?.claims.preferred_username;
+    console.log(usernameofcc);
+    console.log(authState?.accessToken?.claims.userType);
 
-  return (
-    <div>
-        <div className=' container'>
-            <div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        
-                        </tr>
-                    </tbody>
-                </table>
+    useEffect(() => {
+        fetchData();
+    }, [usernameofcc]);
+
+    const fetchData = async () => {
+        try {
+            console.log(usernameofcc)
+
+            const response = await axios.get(`http://localhost:9090/api/courses/getccregcourses/${usernameofcc}`);
+            if (response.data.code !== '00') {
+                throw new Error('Network response was not ok');
+            }
+            const data = response.data.content;
+            setCourses(data);
+            console.log(data)
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+            console.error('Error details:', error.response);
+        }
+    };
+
+    return (
+        <div>
+            <NavBarCC />
+            <div className='container' style={{marginTop:"70px"}}>
+                <div className='row g-3 my-4'>
+                    {courses.map((course, index) => (
+                        <div className="card shadow m-4" style={{width: "18rem"}} key={index}>
+                            <div className="card-body">
+                                <h5 className="card-title py-2">{course.course_id}</h5> 
+                                <h6 className='card-title py-1'>{course.course_name}</h6> 
+                                <a className="btn btn-primary btn-sm mt-2" onClick={`/caeli/${course.course_id}/${course.course_name}`}>To CA Eligibility</a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
