20bd84e9b39aa7b787dd2a78f1e8707e2482f115
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-06-27 10:07:43 +0530

    Update ViewResultBoard.jsx

--- src/layouts/AR/ViewResultBoard/ViewResultBoard.jsx
+++ src/layouts/AR/ViewResultBoard/ViewResultBoard.jsx
@@ -18,6 +18,22 @@ export default function ViewResultBoard() {
     const [availableCourseList, setAvailableCourseList] = useState([]); //State to store the course list that can be added to the result board
     const [availablCoordinators, setAvailableCoordinators] = useState([]); //State to store the available coordinators 
 
+    const [selectedCourse, setSelectedCourse] = useState('0'); //State to store the selected course
+    const [selectedCoordinator, setSelectedCoordinator] = useState('0'); //State to store the selected coordinator
+
+
+
+
+
+    const handleCoordinatorSelection = (selectedCoordinator)=>{       //Function to handle the coordinator selection
+        selectedCoordinator(selectedCoordinator.target.value); //Set the selected coordinator
+    }
+    
+    const handleCourseSelection = (selectedCourse)=>{       //Function to handle the course selection 
+        
+        setSelectedCourse(selectedCourse.target.value); //Set the selected course
+   
+    }
 
 
 
@@ -46,8 +62,55 @@ export default function ViewResultBoard() {
 
   return (
     <div className='view-result-board-body container'>
+
+        <div className='row justify-content-between'>
+
+
+
+            <div className='col-4 div1'>        {/*Left div*/}
+
+                
+                <div className="row justify-content-between">       {/* 1st row of left div*/}
+                    
+                    <div className='col selection-box-col' >
+
+                        <select className='coordinator-select' value ={selectedCoordinator} onChange={handleCoordinatorSelection}>
+                            <option value='0' disabled> Select a coordinator</option>
+                        
+                        </select>
+
+                    </div>
+
+                    <div className='col selection-box-col' >
+
+                        <select className='course-select' value ={selectedCourse} onChange={handleCourseSelection}>
+                            <option value='0' disabled> Select a course</option>
+                            {
+                                availableCourseList.map((course,index)=>(
+                                    <option key={index} value={course.course_id}>{course.course_id} - {course.course_name} - {index}</option>
+                                ))
+                            }
+                        </select>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+
+            <div className='col-4 div2'>
+                Hello
+            </div>
+
+
+
+        </div>
         
-        
+
         <ToastContainer/>
     </div>
   )
